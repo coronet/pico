63040b9d7fb553847439e69d9f87be8868f37acb
David Murray <fernomac@coronet.io>
2015-08-05 21:22:57 -0700

    AbstractPCollection

---
+++ src/main/java/io/coronet/pico/AbstractPCollection.java
@@ -0,0 +1,37 @@
+package io.coronet.pico;
+
+import java.util.Iterator;
+
+/**
+ * Abstract implementation of the {@code PCollection} interface. Adds
+ * a standard implementation of {@link Object#toString()}, since all the
+ * other methods with default implementations are implemented on the interface.
+ */
+public abstract class AbstractPCollection<E> implements PCollection<E> {
+
+    @Override
+    public String toString() {
+        Iterator<E> iter = iterator();
+        if (!iter.hasNext()) {
+            return "[]";
+        }
+
+        StringBuilder builder = new StringBuilder();
+        builder.append('[');
+
+        for (;;) {
+            E e = iter.next();
+            if (e == this) {
+                builder.append("(this collection)");
+            } else {
+                builder.append(e);
+            }
+
+            if (!iter.hasNext()) {
+                return builder.append(']').toString();
+            }
+
+            builder.append(',').append(' ');
+        }
+    }
+}
